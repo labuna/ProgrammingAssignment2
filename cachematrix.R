1f372ef334078cd0a53239982633ff454d71394a
Andrew Labunka <andrew.labunka@pfizer.com>
2016-08-07 09:47:16 -0400

    Coursera Homework commit
    This is a final commit for the homework assignment and contains an
    implementation of makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cachematrix.r provides a set of functions to allow
+## the user to cache the results of matrix inversion
 
-## Write a short comment describing this function
+## Make CacheMatrix creates an object that can
+## 'Set' - store a matrix
+## 'Get'- return the stored matrix
+## 'Set inverse' - cache the inverse of the matrix 'set'
+## 'Get inverse' - return the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the makeCacheMatrix object
+## has a inverse matrix cached. If there is an inverse matrix
+## cached then it returns it, else it solves and caches it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
